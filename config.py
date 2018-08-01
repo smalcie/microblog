fb006e026016569a5dd8aa0bebb1e4bd23f6dec7
smalcie <smalcie66@gmail.com>
2018-08-01 21:27:12 +1200

    heroku deployment

--- config.py
+++ config.py
@@ -20,5 +20,6 @@ class Config(object):
     LANGUAGES = ['en', 'es']
     TRANSLATOR_KEY = os.environ.get('TRANSLATOR_KEY')
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
 
 
